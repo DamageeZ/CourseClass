31a526610d1ff1d8844b1d07b281ce833baba367
zhouzhm1 <zhouzhm1@outlook.com>
2020-11-03 19:19:41 +0800

    第五章之二

---
+++ 验证哥德巴赫猜想之一.c
@@ -0,0 +1,36 @@
+﻿#include<stdio.h>
+#include<math.h>
+
+int isPrime(int a)
+{
+	if(a == 2) return 1;
+	for(int i=2;i<=sqrt(a);i++)
+	{
+		if(a%i==0)
+		{
+			return 0;
+			break;
+		}
+	}
+	return 1;
+}
+
+int main()
+{
+	int count = 0;
+	for(int i =4;i<=2000;i+=2)
+	{
+		for(int j = 2;j<= i/2 ;j++)
+		{
+			if(isPrime(j)&&isPrime(i-j))
+			{
+				printf("%4d=%4d+%4d",i,j,i-j);
+				count++;
+				break;
+			}
+		}
+		if(count % 4 == 0) printf("\n");
+		else printf(" ");
+	}
+	return 0;
+}
