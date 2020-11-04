6ccd375f3d363f07af2005f92f88a2fb64c3ee7a
zhouzhm1 <zhouzhm1@outlook.com>
2020-11-04 19:27:52 +0800

    空间换时间可还行

---
+++ 计数排序.c
@@ -0,0 +1,35 @@
+﻿#include<stdio.h>
+short temp[1000000]={0};
+int Findmax(int ori[],int n)
+{
+	int maxx = -1;
+	for(int i =0;i<n;i++)
+	{
+		if(ori[i]>maxx) maxx = ori[i];
+	}
+	return maxx;
+}
+
+int main()
+{
+	int n,maxx;
+	long a[2002];
+	scanf("%d",&n);
+	for(int i =0;i<n;i++)
+	{
+		scanf("%ld",&a[i]);
+		temp[a[i]] +=1;
+	}
+	maxx = Findmax(a,n);
+	for(int i = 0;i<=maxx;)
+	{
+		if(temp[i]==0)
+		{
+			i++;
+			continue;
+		}
+		printf("%d ",i);
+		temp[i] -= 1;
+	}
+	return 0;
+}
